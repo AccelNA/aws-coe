02d6f72bcb6ea4f5f46105f03390e8382791ecc8
Jagadeesh Puthukkudi <phpjagadeesh@users.noreply.github.com>
2014-11-28 12:16:02 +0530

    Update footer.php

--- demos/user-registration/includes/footer.php
+++ demos/user-registration/includes/footer.php
@@ -1,8 +1,8 @@
 <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+ <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+ <!-- Bootstrap Core JavaScript -->
+ <script src="js/bootstrap.min.js"></script>
     
 <!-- Jquery validation -->
 <script src="js/jquery.validate.js"></script>
