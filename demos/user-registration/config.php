a6f0756a9027c765bf04d6b369788cac7ef8250e
valery-zhurbenko <valery@glide.me>
2015-11-02 17:03:22 +0200

    Create config.php

---
+++ demos/user-registration/config.php
@@ -0,0 +1,14 @@
+<?php
+     require 'vendor\autoload.php';
+     use Aws\Common\Enum\Region;
+     use Aws\DynamoDb\Enum\Type;
+
+     $bucket='user-profile-bucket';  #Replace with your Bucket name 
+     $tableName = "usr_reg"; #Replace with your Database table name
+     $config = array(
+        'key'    => 'AKIAITVNGGCJNTLL7EHA',
+        'secret' => 'Fx+Pjl/B6Tc+Js02qaZSt9GkVvh3q2Fa8yQXSAtL',
+        'profile' => 'default',
+        'region' => Region::US_EAST_1 #replace with your desired region     
+        );
+?>
